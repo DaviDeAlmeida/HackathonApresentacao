95ba1e51dc063ffdd05ab8041ce2325354b7974c
Vinicius Gonzalez <vinicius.gonzales@delage.com.br>
2019-12-17 11:16:05 -0300

    update cores

--- src/components/pages/admin/ocorrenciaChamados/OcorrenciaChamados.jsx
+++ src/components/pages/admin/ocorrenciaChamados/OcorrenciaChamados.jsx
@@ -65,7 +65,7 @@ class OcorrenciaChamados extends Component {
 
     ];
 
-    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#DD8042', '#CC8042'];
 
     const RADIAN = Math.PI / 180;
     const renderCustomizedLabel = ({
